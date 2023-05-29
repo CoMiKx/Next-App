cb58233d98bddb82daca61b8950286305ebc45e7
CoMiKx <su.baramee_st@tni.ac.th>
2023-05-29 23:10:40 +0900

    new file added

--- app/about/page.js
+++ app/about/page.js
@@ -1,8 +1,8 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './../page.module.css'
 import Link from 'next/link';
 
-export default function Home() {
+export default function About() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
